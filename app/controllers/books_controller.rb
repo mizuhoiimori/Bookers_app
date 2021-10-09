fcdbc0de387c21c19209690400a4034d17b2779b
mizuhoiimori <tanaka.mizuho.com@gmail.com>
2021-10-09 10:54:47 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,30 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to :books_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
